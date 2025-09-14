0d588fd1f12e4c001671555ecebea64499495351
HashenWatawala <hashanewatawala@gmail.com>
2025-09-14 18:10:33 +0530

    add error handling constants and enhance error handler middleware

--- middleware/errorhandler.js
+++ middleware/errorhandler.js
@@ -1,6 +1,43 @@
+const { constants } = require("../constants");
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
-    res.json({message: err.message,stackTrace: err.stack});
-}
+    switch (statusCode) {
+        case constants.VALIDATION_EROOR:
+            res.json({
+                title: "Validation Failed",
+                message: err.message,
+                stackTrace: err.stack,
+            });    
+
+            break;
+        case constants.NOT_FOUND:
+            res.json({
+                title: "Not Found",
+                message: err.message,
+                stackTrace: err.stack,
+            });
+            break;
+        case constants.UNAUTHORIZED:
+            res.json({
+                title: "Unauthorized",
+                message: err.message,
+                stackTrace: err.stack,
+            });
+            break;
+        case constants.FORBIDDEN:
+            res.json({
+                title: "Forbidden",
+                message: err.message,
+                stackTrace: err.stack,
+            });    
+        
+        default:
+            console.log("No Error, All is fine");
+            break;
+    }
+
+    res.json({title: "Not Found", message: err.message,stackTrace: err.stack});
+    res.json({title: "Vlidation Failed", message: err.message,stackTrace: err.stack});
+};
 
 module.exports =  errorHandler ;

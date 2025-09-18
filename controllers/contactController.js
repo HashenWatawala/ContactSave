eea0237ec69edd8b42beeb49e7f0dc010dcf1703
HashenWatawala <hashanewatawala@gmail.com>
2025-09-18 10:28:40 +0530

    Enhance createContact function to return the created contact object in response

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -21,7 +21,12 @@ const createContact = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error("All fields are mandatory!");
     }
-    res.status(201).json({message:'Create a contact'});
+    const contact = await Contact.create({
+        name,
+        email,
+        phone
+    });
+    res.status(201).json(contact);
 });
 
 module.exports = {
